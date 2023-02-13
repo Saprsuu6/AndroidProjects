be0b013eed15e8b873041db038ea48b45a7048c4
Andry Saprigin <coffeei.2002@gmail.com>
2023-02-13 16:14:53 +0200

    sturturize project

--- app/src/main/java/step/learning/basics/Game2048/Game2048Activity.java
+++ app/src/main/java/step/learning/basics/Game2048/Game2048Activity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -23,7 +25,6 @@ public class Game2048Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //CalcActivity.context = super.getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2048);
 
@@ -34,19 +35,26 @@ public class Game2048Activity extends AppCompatActivity {
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
                 tvCells[i][j] = findViewById(   // идентификаторы ячеек "cell" + i + j
-                        getResources().getIdentifier("cell" + i + j, "id", getPackageName()));
+                        getResources().getIdentifier(
+                                "cell" + i + j,
+                                "id",
+                                getPackageName()
+                        )
+                );
             }
         }
 
         findViewById(R.id.layout_2048).setOnTouchListener(new OnSwipeListener(Game2048Activity.this) {
             @Override
             public void OnSwipeRight() {
-                Toast.makeText(Game2048Activity.this, "Right", Toast.LENGTH_SHORT).show();
+                if (MoveRight()) SpawnCell();
+                else
+                    Toast.makeText(Game2048Activity.this, "No Right Move", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void OnSwipeLeft() {
-                if (moveLeft()) SpawnCell();
+                if (MoveLeft()) SpawnCell();
                 else
                     Toast.makeText(Game2048Activity.this, "No Left Move", Toast.LENGTH_SHORT).show();
             }
@@ -61,6 +69,8 @@ public class Game2048Activity extends AppCompatActivity {
                 Toast.makeText(Game2048Activity.this, "Bottom", Toast.LENGTH_SHORT).show();
             }
         });
+
+        SpawnCell();
     }
 
     // добавление новой ячейки (с вероятностью 0.9 - 2, 0.1 - 4)
@@ -74,62 +84,90 @@ public class Game2048Activity extends AppCompatActivity {
                 }
             }
         }
-
         // проверяем, есть ли вообще пустые ячейки
         int cnt = freeCells.size();
         if (cnt == 0) return false;
-
         // генерируем случайный индекс
         int rnd = random.nextInt(cnt);
         // генерируем значение для ячейки
         int x = freeCells.get(rnd) / 10;   // разделяем координаты сохраненной ячейки
         int y = freeCells.get(rnd) % 10;
-
         cells[x][y] = random.nextInt(10) == 0 ? 4 : 2;
         // запускаем анимацию на измененной ячейке
         tvCells[x][y].startAnimation(spawnAnimation);
         // запускаем перерисовку поля
-        showField();
-
+        ShowField();
         return true;
     }
 
     // отображение поля (ячеек) после пересчета их значений
-    private void showField() {
+    private void ShowField() {
         Resources resources = getResources();
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
-                tvCells[i][j].setText(String.valueOf(cells[i][j]));
-                tvCells[i][j].setTextAppearance(resources.getIdentifier("Cell_" + cells[i][j], "style", getPackageName()));
-                tvCells[i][j].setBackgroundColor(resources.getColor(resources.getIdentifier("game_bg_" + cells[i][j], "color", getPackageName()), getTheme()));
+                String value = cells[i][j] > 0 ? String.valueOf(cells[i][j]) : "";
+
+                tvCells[i][j].setText(String.valueOf(value));
+                tvCells[i][j].setTextAppearance(      // R.style.Cell_2 ) ;
+                        resources.getIdentifier(
+                                "Cell_" + cells[i][j],
+                                "style",
+                                getPackageName()
+                        ));
+                tvCells[i][j].setBackgroundColor(
+                        resources.getColor(
+                                resources.getIdentifier(
+                                        "game_bg_" + cells[i][j],
+                                        "color",
+                                        getPackageName()
+                                ),
+                                getTheme()
+                        ));
             }
         }
     }
 
-    private boolean moveLeft() {
-//        boolean result = false;
-//        for (int i = 0; i < 4; ++i) {
-//            boolean needRepeat = true;
-//            while (needRepeat) {
-//                needRepeat = false;
-//                for (int j = 0; j < 3; ++j) {
-//                    if (cells[i][j] == 0) {
-//                        for (int k = j + 1; k < 4; ++k) {
-//                            if (cells[i][k] != 0) {
-//                                cells[i][k - 1] = cells[i][k];
-//                                cells[i][k] = 0;
-//                                needRepeat = true;
-//                                result = true;
-//                            }
-//                        }
-//                        cells[i][3] = 0;
-//                    }
-//                }
-//            }
-//        }
-//        if (result) showField();
-//        return result;
+    private boolean MoveLeft() {
+        boolean result = false;
 
-        return true;
+        for (int i = 0; i < 4; ++i) {
+            int j = 0;
+            if (cells[i][j] == 0) {
+                for (int k = j + 1; k < 4; ++k) {
+                    if (cells[i][k] != 0) {
+                        cells[i][k - 1] = cells[i][k];
+                        cells[i][k] = 0;
+                        result = true;
+                    }
+                }
+
+                cells[i][3] = 0;
+            }
+        }
+
+        if (result) ShowField();
+        return result;
+    }
+
+    private boolean MoveRight() {
+        boolean result = false;
+
+        for (int i = 3; i > 0; --i) {
+            int j = 3;
+            if (cells[i][j] == 0) {
+                for (int k = j - 1; k > 0; --k) {
+                    if (cells[i][k] != 0) {
+                        cells[i][k + 1] = cells[i][k];
+                        cells[i][k] = 0;
+                        result = true;
+                    }
+                }
+
+                cells[i][0] = 0;
+            }
+        }
+
+        if (result) ShowField();
+        return result;
     }
 }
