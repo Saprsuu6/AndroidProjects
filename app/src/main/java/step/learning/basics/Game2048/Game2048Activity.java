605b5e2bf659ef66969ecb268ee2085261b34fd0
AndrySaprigin <coffeei.2002@gmail.com>
2023-02-13 14:14:03 +0300

    add move left logic

--- app/src/main/java/step/learning/basics/Game2048/Game2048Activity.java
+++ app/src/main/java/step/learning/basics/Game2048/Game2048Activity.java
@@ -1,22 +1,42 @@
 package step.learning.basics.Game2048;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import step.learning.basics.Calculator.Buttons;
-import step.learning.basics.Calculator.CalcActivity;
-import step.learning.basics.Calculator.Views;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import step.learning.basics.R;
 
 public class Game2048Activity extends AppCompatActivity {
+    private int[][] cells = new int[4][4];
+    private TextView[][] tvCells = new TextView[4][4];
+    private final Random random = new Random();
+    private Animation spawnAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //CalcActivity.context = super.getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2048);
+
+        // загрузка анимации как ресурса
+        spawnAnimation = AnimationUtils.loadAnimation(this, R.anim.spawn_cell);
+        spawnAnimation.reset();
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                tvCells[i][j] = findViewById(   // идентификаторы ячеек "cell" + i + j
+                        getResources().getIdentifier("cell" + i + j, "id", getPackageName()));
+            }
+        }
 
         findViewById(R.id.layout_2048).setOnTouchListener(new OnSwipeListener(Game2048Activity.this) {
             @Override
@@ -26,7 +46,9 @@ public class Game2048Activity extends AppCompatActivity {
 
             @Override
             public void OnSwipeLeft() {
-                Toast.makeText(Game2048Activity.this, "Left", Toast.LENGTH_SHORT).show();
+                if (moveLeft()) SpawnCell();
+                else
+                    Toast.makeText(Game2048Activity.this, "No Left Move", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -39,5 +61,75 @@ public class Game2048Activity extends AppCompatActivity {
                 Toast.makeText(Game2048Activity.this, "Bottom", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // добавление новой ячейки (с вероятностью 0.9 - 2, 0.1 - 4)
+    private boolean SpawnCell() {
+        // собираем все пустые ячейки
+        List<Integer> freeCells = new ArrayList<>();
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                if (cells[i][j] == 0) {
+                    freeCells.add(i * 10 + j);   // сохраняем координаты свободной ячейки одним числом
+                }
+            }
+        }
+
+        // проверяем, есть ли вообще пустые ячейки
+        int cnt = freeCells.size();
+        if (cnt == 0) return false;
+
+        // генерируем случайный индекс
+        int rnd = random.nextInt(cnt);
+        // генерируем значение для ячейки
+        int x = freeCells.get(rnd) / 10;   // разделяем координаты сохраненной ячейки
+        int y = freeCells.get(rnd) % 10;
+
+        cells[x][y] = random.nextInt(10) == 0 ? 4 : 2;
+        // запускаем анимацию на измененной ячейке
+        tvCells[x][y].startAnimation(spawnAnimation);
+        // запускаем перерисовку поля
+        showField();
+
+        return true;
+    }
+
+    // отображение поля (ячеек) после пересчета их значений
+    private void showField() {
+        Resources resources = getResources();
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                tvCells[i][j].setText(String.valueOf(cells[i][j]));
+                tvCells[i][j].setTextAppearance(resources.getIdentifier("Cell_" + cells[i][j], "style", getPackageName()));
+                tvCells[i][j].setBackgroundColor(resources.getColor(resources.getIdentifier("game_bg_" + cells[i][j], "color", getPackageName()), getTheme()));
+            }
+        }
+    }
+
+    private boolean moveLeft() {
+//        boolean result = false;
+//        for (int i = 0; i < 4; ++i) {
+//            boolean needRepeat = true;
+//            while (needRepeat) {
+//                needRepeat = false;
+//                for (int j = 0; j < 3; ++j) {
+//                    if (cells[i][j] == 0) {
+//                        for (int k = j + 1; k < 4; ++k) {
+//                            if (cells[i][k] != 0) {
+//                                cells[i][k - 1] = cells[i][k];
+//                                cells[i][k] = 0;
+//                                needRepeat = true;
+//                                result = true;
+//                            }
+//                        }
+//                        cells[i][3] = 0;
+//                    }
+//                }
+//            }
+//        }
+//        if (result) showField();
+//        return result;
+
+        return true;
     }
 }
